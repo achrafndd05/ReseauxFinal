f0a6acb37a2d48568d7f54e214fa1789d93754bf
achraf derrrar <an.derrar@esi-sba.dz>
2022-06-13 22:54:22 +0100

    finalllll

---
+++ client/src/page.jsx
@@ -0,0 +1,219 @@
+import React, { useState } from 'react';
+
+import './App.css'
+import Topology from './topology.webp'
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+export default function Page() {
+
+
+
+    const [openR1, setOpenR1] = React.useState(false);
+    const [openR2, setOpenR2] = React.useState(false);
+    const handleClickOpenR1 = () => {
+        setOpenR1(true);
+    };
+
+    const handleCloseR1 = () => {
+        setOpenR1(false);
+    };
+
+    const handleClickOpenR2 = () => {
+        setOpenR2(true);
+    };
+
+    const handleCloseR2 = () => {
+        setOpenR2(false);
+    };
+    const [R1Name, setR1Name] = useState("");
+    const [R2Name, setR2Name] = useState("");
+
+
+    // const handleR2Change = (event) => {
+    //     setR2Name(event.target.value);
+    // };
+
+    const handleRouter1Name = (e) => {
+        e.preventDefault()
+        fetch(`http://localhost:8080/router1_name?name=${R1Name}`)
+            .then(
+                res => res.json()
+            ).then(
+                data => {
+                    console.log(data.result)
+                }
+            )
+    }
+
+    const handleRouter1Config = (e) => {
+
+        e.preventDefault()
+
+        fetch("http://localhost:8080/router1_config")
+            .then(
+                res => res.json()
+            ).then(
+                data => {
+                    console.log(data.result)
+                }
+            )
+
+    }
+
+
+    const handleRouter2Name = (e) => {
+        e.preventDefault()
+        fetch(`http://localhost:8080/router1_name?name=${R2Name}`)
+            .then(
+                res => res.json()
+            ).then(
+                data => {
+                    console.log(data.result)
+                }
+            )
+    }
+
+    const handleRouter2Config = (e) => {
+
+        e.preventDefault()
+
+        fetch("http://localhost:8080/router2_config")
+            .then(
+                res => res.json()
+            ).then(
+                data => {
+                    console.log(data.result)
+                }
+            )
+
+    }
+
+    return (
+
+
+        <div className="page-container">
+            <div className="App-header">
+                <h1>RIP Configuration</h1>
+            </div>
+            <div className="content">
+                <div className="buttonsplace" >
+
+                    <div className="R-container">
+                        <Button variant="outlined" className="buttonR1" onClick={handleClickOpenR1}>Changer Nom de Router 1</Button>
+                        <Dialog
+
+                            open={openR1} onClose={handleCloseR1}>
+
+                            <DialogTitle>Changer Nom de Router 1</DialogTitle>
+
+                            <DialogContent>
+                                <form
+                                    onSubmit={(e) => {
+
+                                        handleRouter1Name(e)
+
+                                    }}
+                                >
+                                    <DialogContentText>
+                                        Pour changer le Nom click Confirmer et pour annuler click Annuler
+                                    </DialogContentText>
+                                    <TextField
+                                        autoFocus
+                                        margin="dense"
+                                        id="name"
+                                        label="Router Name"
+                                        value={R1Name}
+                                        fullWidth
+                                        variant="standard"
+                                        onChange={(e) => {
+                                            setR1Name(e.target.value);
+                                            // handleR2Change(R2);
+                                        }}
+                                    />
+                                    <DialogActions>
+                                        <Button onClick={handleCloseR1}>Annuler</Button>
+                                        <Button type="submit"  >Confirmer </Button>
+                                    </DialogActions>
+                                </form>
+                            </DialogContent>
+
+
+
+                        </Dialog>
+
+
+                        <Button sx={{
+                            marginTop: 2
+                        }}
+                            variant="outlined" className="buttonR1" onClick={(e) => handleRouter1Config(e)} >Configurer Rip Router 1</Button>
+                    </div>
+                    {/*
+                    R2 Router
+                    */}
+
+
+                    <div className="R-container">
+
+                        <Button variant="outlined" className="buttonR1" onClick={handleClickOpenR2} >Changer Nom de Router 2</Button>
+
+                        <Dialog
+                            open={openR2} onClose={handleCloseR2}>
+                            <DialogTitle>Changer Nom de Router 2</DialogTitle>
+                            <DialogContent>
+                                <form action=""
+                                    onSubmit={(e) => {
+
+                                        handleRouter2Name(e)
+
+                                    }}
+
+                                >
+                                    <DialogContentText>
+                                        Pour changer le Nom click Confirmer et pour annuler click Annuler
+
+                                    </DialogContentText>
+                                    <TextField
+                                        name="R2"
+                                        value={R2Name}
+                                        autoFocus
+                                        margin="dense"
+                                        id="name"
+                                        label="Router Name"
+                                        fullWidth
+                                        variant="standard"
+                                        onChange={(e) => {
+                                            setR2Name(e.target.value);
+                                            // handleR2Change(R2);
+                                        }}
+                                    />
+                                    <DialogActions>
+                                        <Button onClick={handleCloseR2}>Cancel</Button>
+                                        <Button type="submit">Confirmer</Button>
+                                    </DialogActions>
+                                </form>
+                            </DialogContent>
+
+                        </Dialog>
+
+                        <Button
+                            sx={{
+                                marginTop: 2
+                            }}
+                            variant="outlined" className="buttonR1" onClick={(e) => handleRouter2Config(e)} >Configurer Rip Router 2</Button>
+                    </div>
+                </div>
+                <div className="imageplace">
+                    <img src={Topology} alt="Logo" />
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
---
+++ server/app.py
@@ -0,0 +1,137 @@
+from flask import Flask,render_template,request
+import getpass
+import sys
+import telnetlib
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("app.js")
+
+@app.route("/router1_name", methods=['GET'])
+def router1_name():
+
+            # output = request.form.to_dict()
+            # print(output)
+            # name = output["name"]
+    
+            # HOST = "192.168.56.10"
+            # user = input("Enter votre tlnet username:")
+            # password = getpass.getpass()
+
+            # tn = telnetlib.Telnet(HOST)
+
+            # tn.read_until(b"Username")
+            # tn.write(user.encode("ascii") + b"\n")
+            # if password:
+            #     tn.read_until(b"Password", timeout=None)
+            #     tn.write(password.encode("ascii") + b"\n")
+
+
+            # tn.write(b"enable\n")
+            # tn.write(b"conf t\n")
+            # tn.write(b"hostname {}\n".format(name))
+            # tn.write(b"exit\n")
+
+            # print(tn.read_all())
+
+            args = request.args
+
+            return {"result": args.get("name")}
+
+@app.route("/router1_config")
+def router1_config():
+
+            # output = request.form.to_dict()
+            # print(output)
+            # name = output["name"]
+    
+            # HOST = "192.168.56.10"
+            # user = input("Enter votre tlnet username:")
+            # password = getpass.getpass()
+
+            # tn = telnetlib.Telnet(HOST)
+
+            # tn.read_until(b"Username")
+            # tn.write(user.encode("ascii") + b"\n")
+            # if password:
+            #     tn.read_until(b"Password", timeout=None)
+            #     tn.write(password.encode("ascii") + b"\n")
+
+
+            # tn.write(b"enable\n")
+            # tn.write(b"conf t\n")
+            # tn.write(b"hostname {}\n".format(name))
+            # tn.write(b"exit\n")
+
+            # print(tn.read_all())
+
+            return {"result": "ROUTER1 CONFIG!!!!!!!"}          
+
+
+@app.route("/router2_name", methods=['GET'])
+def router2_name():
+
+            # output = request.form.to_dict()
+            # print(output)
+            # name = output["name"]
+    
+            # HOST = "192.168.43.47"
+            # user = "zaki"
+            # password = "123"
+
+            # tn = telnetlib.Telnet(HOST)
+
+            # #tn.read_until(b"Username")
+            # tn.write(user.encode("ascii") + b"\n")
+            # #if password:
+            #     #tn.read_until(b"Password", timeout=None)
+            # tn.write(password.encode("ascii") + b"\n")
+
+            # #configuration commands
+            # tn.write(b"enable\n")
+            # tn.write(b"conf t\n")
+            # tn.write(b"hostname {}\n".format(name))
+            # tn.write(b"exit\n")
+
+            # print(tn.read_all())
+
+            args = request.args
+
+            return {"result": args.get("name")}
+
+@app.route("/router2_config")
+def router2_config():
+
+    
+            # HOST = "localhost"
+            # user = "zaki"
+            # password = "123"
+            # tn = telnetlib.Telnet(HOST)
+
+            # #tn.read_until(b"Username")
+            # tn.write(user.encode("ascii") + b"\n")
+            # #if password:
+            #     #tn.read_until(b"Password", timeout=None)
+            # tn.write(password.encode("ascii") + b"\n")
+
+
+            # tn.write(b"enable\n")
+            # tn.write(b"conf t\n")
+            # tn.write(b"hostname {}\n".format(name))
+            # tn.write(b"exit\n")
+
+            # print(tn.read_all())
+
+            return {"result": "ROUTER2 CONFIG!!!!!!!"}                      
+
+    
+        
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port = 8080)
